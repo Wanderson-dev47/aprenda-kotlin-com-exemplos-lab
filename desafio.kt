46c1ae687b3edd7cb2a2e454eeeac1844490ee78
Wanderson Brito <151024253+Wanderson-dev47@users.noreply.github.com>
2024-02-25 22:35:02 -0300

    Meu código

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,99 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+class Usuario(val nomeDoUsuario: String) {
+    var totalDeXP: Int = 0
+    val UPAR_XP: Int = 30
+    var cursosMatriculados: Int = 0
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun progredirXP() {  
+        println("Progredindo XP de $nomeDoUsuario...")
+        totalDeXP += UPAR_XP      
     }
+    
+    fun contaCursoMatriculado() {
+        cursosMatriculados += 1
+	}
 }
 
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+
+data class ConteudoEducacional(var nome: String, val duracao: Int) {
+    
+}
+
+data class Formacao(val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
+
+    val tiposDeConteudos = mutableListOf<Formacao>()
+    init {
+        tiposDeConteudos.add(this)
+    }
+    
+    val inscritos = mutableListOf<Usuario>()
+    
+    fun obterStringConteudos(): String {
+        return conteudos.joinToString("\n") { conteudo ->
+            "Nome do Curso: ${conteudo.nome}\nCarga Horária: ${conteudo.duracao}\nNível: $nivel"
+        }
+    }
+    
+    fun matricular(usuario: Usuario) {
+        // Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).
+        inscritos.add(usuario)
+        println("\nUsuário matriculado com sucesso! \nNome: ${usuario.nomeDoUsuario} \nXP do Usuário: ${usuario.totalDeXP} \nCurso Matriculado -> ${obterStringConteudos()}")
+    }
+    
+   
+}
+
+
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
+    //ADICIONANDO CURSOS
+    val formacao1 = Formacao(
+    nivel = Nivel.BASICO,
+    conteudos = listOf(ConteudoEducacional("Bootcamp TQI Kotlin - Backend Developer", 112))
+	)
+    
+    val formacao2 = Formacao(
+    nivel = Nivel.INTERMEDIARIO,
+    conteudos = listOf(ConteudoEducacional("TQI Fullstack Developer", 90))
+	)
+    
+    val formacao3 = Formacao(
+    nivel = Nivel.AVANCADO,
+    conteudos = listOf(ConteudoEducacional("Formação Quality Assurance (QA)", 60))
+	)
+    
+    println("\n_-------Fazendo testes---------_\n")
+    println(formacao1.tiposDeConteudos)
+    println(formacao2.tiposDeConteudos)
+    println(formacao3.tiposDeConteudos)
+    
+    //Matriculando alunos:
+    println("\n_-------------------------_\n")
+    
+    println("Lista de formação DIO: \n1 - Bootcamp TQI Kotlin - Backend Developer | Básico") 
+    println("2 - TQI Fullstack Developer | Intermediário")
+    println("3 - Formação Quality Assurance (QA) | Avançado")
+    
+    val usuario1 = Usuario("João")
+    formacao3.matricular(usuario1)
+    usuario1.contaCursoMatriculado()
+    
+    val usuario2 = Usuario("Maria")
+    formacao2.matricular(usuario2)
+    usuario2.contaCursoMatriculado()
+    
+    val usuario3 = Usuario("André")
+    formacao1.matricular(usuario3)
+    usuario3.contaCursoMatriculado()
+    
+    println()
+    usuario1.progredirXP()
+    usuario3.progredirXP()
+    usuario1.progredirXP()
+    
+    
+    formacao2.matricular(usuario1)
+    usuario1.contaCursoMatriculado()
+    
+
 }
